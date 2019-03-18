6fb7d611cb2e6556bae44c31ab3951741b6a749e
JustTapia <38672129+JustTapia@users.noreply.github.com>
2019-03-18 15:44:35 -0600

    Intento de enviar y responder servidor

--- udpClient.c
+++ udpClient.c
@@ -1,28 +1,3 @@
-/*
-void recibirMensajes(int conexion, struct sockaddr_in servaddr) {
-    char mensaje[100];
-    //while (1) {
-        int recibir = recvfrom(conexion,mensaje,sizeof(mensaje),0,(struct sockaddr *)&servaddr, &sin_size);
-        if(recibir >0){
-            printf("%s\n", mensaje);
-        }
-    //}
-}
-
-void enviarMensajes(int conexion, struct sockaddr_in servaddr) {
-    int n;
-    char mensaje[100];
-    //while(1){
-        n=0;
-        while ((mensaje[n++] = getchar()) != '\n');
-        sendto(conexion,mensaje,sizeof(mensaje),0,(struct sockaddr *)&servaddr,sin_size);
-        if ((strncmp(mensaje, "exit", 4)) == 0) { 
-            printf("Client Exit...\n"); 
-        }
-    //}
-}
-*/
-
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <unistd.h> 
@@ -33,14 +8,142 @@ void enviarMensajes(int conexion, struct sockaddr_in servaddr) {
 #include <netinet/in.h> 
 #include<netdb.h>
 
-#define PORT	 5555
+#define PORT 5555
 
+
+void recibirMensajes(int conexion, struct sockaddr_in servaddr, socklen_t sin_size) {
+    char mensaje[512];
+    while (1) {
+        int recibir = recvfrom(conexion,mensaje,sizeof(mensaje),0,(struct sockaddr *)&servaddr, &sin_size);
+        if(recibir >0){
+            printf("%s\n", mensaje);
+        }
+    }
+}
+
+void comandos(int conexion, struct sockaddr_in servaddr, socklen_t sin_size){
+    char mensaje[512];
+    int j = 0, i = -1;
+
+    while(1){
+
+        *mensaje = '\0';
+        i = -1;
+
+        while(*(mensaje+i)!=10){
+        i++;
+        scanf("%c", mensaje+i);
+        }
+
+        *(mensaje+i)='\0';
+
+        if( *mensaje != '-'){
+            printf("Texto Invalido\n");
+
+        }else if( *(mensaje + 1) == 's') { //enviar mensaje, comando -s
+            if( *(mensaje + 2) == ' '){
+                enviarMensaje(mensaje + 3, conexion, servaddr, sin_size);
+            }else{
+                enviarMensaje(mensaje + 2,  conexion, servaddr, sin_size);
+            }
+
+        }else if( *(mensaje + 1) == 'p'){ // cambiar puerto, comando -p
+            if( *(mensaje + 2) == ' '){
+                cambiarPuerto(mensaje + 3, conexion, servaddr, sin_size);
+            }else{
+                cambiarPuerto(mensaje + 2, conexion, servaddr, sin_size);
+            }
+
+        }else if( *(mensaje + 1) == 'q'){   // salir, comando -q
+            printf("Muchas gracias por usar nuestro sistema!\n");
+            break; 
+        }else{
+            printf("Comando Inexistente\n");
+        }
+    }
+}
+
+void enviarMensaje(char * mensaje, int conexion, struct sockaddr_in servaddr, socklen_t sin_size) {
+    int n;
+    //while(1){
+        n=0;
+        while ((mensaje[n++] = getchar()) != '\n');
+        sendto(conexion, mensaje,sizeof(mensaje),0,(struct sockaddr *)&servaddr,sin_size);
+        if ((strncmp(mensaje, "exit", 4)) == 0) { 
+            printf("Client Exit...\n"); 
+        }
+    //}
+}
+
+void cambiarPuerto(char *mensaje, int conexion, struct sockaddr_in servaddr, socklen_t sin_size){
+    char archtext[64];
+    int i = 0, j = 0;
+    FILE *archivo = fopen("arch.conf", "r");
+    while(( *(archtext + i) = fgetc(archivo)) != 10){
+        i++;
+    }
+    *(archtext + i) = '\n';
+    i++;
+
+    while(j<8){
+        *(archtext + i) = fgetc(archivo);
+        j++;
+        i++;
+    }
+
+    j = 0;
+    while(j < 4){
+        *(archtext + i) = *(mensaje + j);
+        i++;
+        j++;
+    }
+
+    *(archtext+i) = '\0';
+    fclose(archivo);
+
+    archivo = fopen("arch.conf", "w");
+    fprintf(archivo, "%s", archtext);
+    fclose(archivo);
+}
 
 int main() { 
-	iint sockfd;
+    char tmp, ip[15], sPuerto[2], nombre[16];
+    int i = -1, j = 0;
+    FILE *archivo = fopen("arch.conf", "r");
+    while(j<4){
+        tmp = fgetc(archivo);
+        j++;
+    }
+    j = 0;
+    while((tmp = fgetc(archivo)) != 10){
+        *(ip+j) = tmp;
+        j++;
+    }
+    *(ip+j) = '\0';
+    j = 0;
+    while(j<8){
+        tmp = fgetc(archivo);
+        j++;
+    }
+    j = 0;
+    while((tmp = fgetc(archivo)) != EOF){
+        *(sPuerto+j) = tmp;
+        j++;
+    }
+    *(sPuerto+j) = '\0';
+    fclose(archivo);
+    printf("Por favor ingrese su nombre de usuario para su registro:\n");
+    while(*(nombre+i)!=10){
+        i++;
+        scanf("%c", nombre+i);
+    }
+    *(nombre+i)='\0';
+
+
+	int sockfd;
     struct sockaddr_in servaddr, cliente;
-    while(1){
-        if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+    //while(1){
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
         perror("UDP Client: ERROR while creating the socket.\n");
         exit(1);
     }
@@ -52,29 +155,20 @@ int main() {
     bzero(&cliente,sizeof(struct sockaddr));
     cliente.sin_family=AF_INET;
     cliente.sin_addr.s_addr=INADDR_ANY;
-    printf("%s\n", argv[1]);
-    cliente.sin_port=htons(atoi(argv[1]));
-    if((bind(sockfd,(struct sockaddr *)&cliente,sizeof(cliente)))!=0){
-        printf("client socket bind failed...\n");
+    cliente.sin_port=htons(atoi(sPuerto));
+
+    socklen_t sin_size = sizeof(struct sockaddr);
+    sendto(sockfd, nombre,sizeof(nombre),0,(struct sockaddr *)&servaddr,sin_size);
+    pid_t childpid =fork();
+    if(childpid == 0){
+        recibirMensajes(sockfd, servaddr, sin_size);
+    }
+    if(childpid >0){
+        comandos(sockfd, servaddr, sin_size);
+    }else{
         exit(0);
     }
-    char nombre[16];
-    strcpy(nombre, argv[2]);
-    printf("Conectado con %s:%d\n",inet_ntoa(servaddr.sin_addr),htons(servaddr.sin_port));
-    sendto(sockfd, nombre,sizeof(nombre),0,(struct sockaddr *)&servaddr,sin_size);
-    pid_t childpid;
-    for(;;){
-        if((childpid = fork()) == 0){
-            recibirMensajes(sockfd, servaddr);
-        }
-        if(enviarMensajes(sockfd, servaddr) == 0){
-            exit(0);
-        }else{
-            break;
-        }
-    }
     close(sockfd);
-    argv[1] = "4444";
-    }
+    //}
 } 
 
