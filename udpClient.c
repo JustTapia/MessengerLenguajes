a7cf944e639e60114e216f8bd68fcaa173f25ff8
Suzy12 <37936067+Suzy12@users.noreply.github.com>
2019-03-18 12:20:27 -0600

    Cliente decide puerto

--- udpClient.c
+++ udpClient.c
@@ -37,41 +37,44 @@ void enviarMensajes(int conexion, struct sockaddr_in servaddr) {
 
 
 int main() { 
-	int socket_fd, n;
-	struct sockaddr_in server_addr;
-	struct hostent *host;
-	host = gethostbyname("127.0.0.1");
+	iint sockfd;
+    struct sockaddr_in servaddr, cliente;
+    while(1){
+        if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        perror("UDP Client: ERROR while creating the socket.\n");
+        exit(1);
+    }
+    bzero(&servaddr,sizeof(struct sockaddr));
+    servaddr.sin_family=AF_INET;
+    servaddr.sin_addr.s_addr= INADDR_ANY; //inet_addr("127.0.0.1");
+    servaddr.sin_port=htons(PORT);
 
-	if ((socket_fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-		perror("UDP Client: ERROR while creating the socket.\n");
-		exit(1);
-	}
-	bzero(&server_addr,sizeof(struct sockaddr));
-    server_addr.sin_family=AF_INET;
-    //servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
-    server_addr.sin_port=htons(PORT);
-    bcopy((char *)host->h_addr, (char *)&server_addr.sin_addr.s_addr, sizeof(host->h_length));
-
-	int count;
-	char buffer[128];
-	pid_t childpid;
-	while (1) {
-		socklen_t sin_size = sizeof(struct sockaddr);
-		if((childpid = fork()) == 0){	
-        	n = recvfrom(socket_fd, buffer, sizeof(buffer), MSG_WAITALL, (struct sockaddr *) &server_addr, &sin_size);
-        	if (n < 0) {
-        		perror("UDP Client: ERROR while reading response from server socket.\n");
-			}else{
-				buffer[n] = '\0';
-				printf("UDP Client: received response -> %s\n", buffer);
-        	} 
+    bzero(&cliente,sizeof(struct sockaddr));
+    cliente.sin_family=AF_INET;
+    cliente.sin_addr.s_addr=INADDR_ANY;
+    printf("%s\n", argv[1]);
+    cliente.sin_port=htons(atoi(argv[1]));
+    if((bind(sockfd,(struct sockaddr *)&cliente,sizeof(cliente)))!=0){
+        printf("client socket bind failed...\n");
+        exit(0);
+    }
+    char nombre[16];
+    strcpy(nombre, argv[2]);
+    printf("Conectado con %s:%d\n",inet_ntoa(servaddr.sin_addr),htons(servaddr.sin_port));
+    sendto(sockfd, nombre,sizeof(nombre),0,(struct sockaddr *)&servaddr,sin_size);
+    pid_t childpid;
+    for(;;){
+        if((childpid = fork()) == 0){
+            recibirMensajes(sockfd, servaddr);
         }
-        char mensaje[100];
-        n=0;
-        while ((mensaje[n++] = getchar()) != '\n');
-        sendto(socket_fd, mensaje, strlen(mensaje), MSG_CONFIRM, (struct sockaddr *) &server_addr, sin_size);
-	}
-	close(socket_fd); 
-	return 0;
+        if(enviarMensajes(sockfd, servaddr) == 0){
+            exit(0);
+        }else{
+            break;
+        }
+    }
+    close(sockfd);
+    argv[1] = "4444";
+    }
 } 
 
